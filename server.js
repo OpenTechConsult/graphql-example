6ee0cb2c4a7d98a3ead6d33a92f2fd98d0842b79
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-17 06:38:43 +0000

    Allow graphiql to be used in the project by setting the graphiql option in the express-graphql middleware

--- server.js
+++ server.js
@@ -19,8 +19,9 @@ const app = express()
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
+    graphiql: true,
 }))
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('Running GraphQL server...')
 })
