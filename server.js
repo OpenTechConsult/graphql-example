5960c8bd34fbf5b2fd3522786d5c76eb270718ac
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-18 09:05:23 +0000

    Resolvers - created two resolver in the makeExecutableSchema function

--- server.js
+++ server.js
@@ -11,7 +11,20 @@ const orders = require('./orders/orders.model')
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'))
 
 const schema = makeExecutableSchema({
-    typeDefs: typesArray
+    typeDefs: typesArray,
+    resolvers: {
+        Query: {
+            products: async (parent, args, context, info) => {
+                console.log('Getting the products...')
+                const product = await Promise(parent.products)
+                return product
+            },
+            orders: (parent, args, context, info) => {
+                console.log('Getting the orders...')
+                return parent.orders
+            }
+        }
+    }
 })
 
 const root = {
