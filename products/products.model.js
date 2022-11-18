b08f464bed8b0459226e556bb2b5212606d04045
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-18 10:55:18 +0000

    Filtering with Queries and Resolvers

--- products/products.model.js
+++ products/products.model.js
@@ -8,14 +8,21 @@ const products = [
         id: 'bluejean',
         description: 'Blue Jeans',
         price: 55.50,
-    },
+    }
 ]
 
 function getAllProducts() {
     return products
 }
 
+function getProductsByPrice(min, max) {
+    return products.filter((product) => {
+        return product.price >= min && product.price <= max
+    })
+}
+
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getProductsByPrice,
 }
 
